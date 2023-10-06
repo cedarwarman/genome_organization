451b5c51a2ae832f58a8df4a463a8294b031a0b3
cedarwarman <cedardalewarman@gmail.com>
2023-10-05 17:03:36 -0700

    Doing a little exploration of the metadata.

--- R/get_sequence_data_size.R
+++ R/get_sequence_data_size.R
@@ -20,3 +20,42 @@ solavar_metadata <- read.table(
   sep = ',',
   header = TRUE
 )
+
+
+# Figuring out the size ---------------------------------------------------
+# It's pretty straightforward for the Solavar samples, as far as I can tell
+# there's nothing extra in here. First getting the size in GB.
+solavar_metadata <- solavar_metadata %>%
+  mutate(size_gb = Bytes / 10^9)
+
+# Total size in TB:
+sum(solavar_metadata$size_gb) / 1000 # 1.43 TB
+
+# For the Alonge long read data there's some extra RNA-seq stuff in there.
+alonge_metadata <- alonge_metadata %>%
+  mutate(size_gb = Bytes / 10^9)
+
+# Total size in TB:
+sum(alonge_metadata$size_gb) / 1000 # 6.79 TB
+
+# Only including the genomic data:
+alonge_metadata %>%
+  filter(LibrarySource == "GENOMIC") %>%
+  summarize(sum_tb = sum(size_gb) / 1000) # 6.74 TB, not much different
+
+# Curious how many libraries there are for each platform and each accession
+platform_summary <- alonge_metadata %>%
+  filter(LibrarySource == "GENOMIC") %>%
+  group_by(Cultivar, Platform) %>%
+  summarize(n = n())
+
+# How many have both long read and Illumina?
+alonge_metadata %>%
+  filter(LibrarySource == "GENOMIC") %>%
+  group_by(Cultivar, Platform) %>%
+  summarize(n = n()) %>%
+  ungroup() %>%
+  group_by(Cultivar) %>%
+  summarize(n_rows = n()) %>%
+  filter(n_rows > 1) %>%
+  nrow() # 14 accessions have both
