b6bef29bdaa1be47ca021e1727ad64f1386ab92e
cedarwarman <cedardalewarman@gmail.com>
2023-10-20 09:25:06 -0700

    Fix names.

--- R/prepare_SRA_ids_for_download.R
+++ R/prepare_SRA_ids_for_download.R
@@ -13,13 +13,13 @@ alonge_metadata <- read.table(
 )
 
 varitome_metadata <- read.table(
-  file = file.path(getwd(), "data", "solavar_metadata.txt"),
+  file = file.path(getwd(), "data", "varitome_metadata.txt"),
   sep = ',',
   header = TRUE
 )
 
 
-# Making a file of Varitome SRA ids
+# Making a file of Varitome SRA ids ---------------------------------------
 write.table(
   varitome_metadata$Run,
   file = file.path(getwd(), "output", "varitome_sra_ids.txt"),
@@ -27,3 +27,17 @@ write.table(
   row.names = FALSE,
   col.names = FALSE
 )
+
+
+# Making a file of Alonge et al SRA ids -----------------------------------
+alonge_metadata_output <- alonge_metadata %>%
+  filter(LibrarySource == "GENOMIC")
+
+write.table(
+  alonge_metadata_output$Run,
+  file = file.path(getwd(), "output", "alonge_sra_ids.txt"),
+  quote = FALSE,
+  row.names = FALSE,
+  col.names = FALSE
+)
+
