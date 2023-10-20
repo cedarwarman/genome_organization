df713ab885ce3bb5590da20035b017a041531d3f
cedarwarman <cedardalewarman@gmail.com>
2023-10-20 09:52:11 -0700

    Start working on the joins.

--- R/prepare_SRA_ids_for_download.R
+++ R/prepare_SRA_ids_for_download.R
@@ -3,6 +3,8 @@
 # fetchngs.
 
 library(dplyr)
+library(googlesheets4)
+gs4_auth(path = "~/.credentials/google_sheets_api/service_account.json")
 
 
 # Loading the data --------------------------------------------------------
@@ -20,24 +22,43 @@ varitome_metadata <- read.table(
 
 
 # Making a file of Varitome SRA ids ---------------------------------------
-write.table(
-  varitome_metadata$Run,
-  file = file.path(getwd(), "output", "varitome_sra_ids.txt"),
-  quote = FALSE,
-  row.names = FALSE,
-  col.names = FALSE
-)
+# write.table(
+#   varitome_metadata$Run,
+#   file = file.path(getwd(), "output", "varitome_sra_ids.txt"),
+#   quote = FALSE,
+#   row.names = FALSE,
+#   col.names = FALSE
+# )
 
 
 # Making a file of Alonge et al SRA ids -----------------------------------
 alonge_metadata_output <- alonge_metadata %>%
   filter(LibrarySource == "GENOMIC")
 
-write.table(
-  alonge_metadata_output$Run,
-  file = file.path(getwd(), "output", "alonge_sra_ids.txt"),
-  quote = FALSE,
-  row.names = FALSE,
-  col.names = FALSE
-)
+# write.table(
+#   alonge_metadata_output$Run,
+#   file = file.path(getwd(), "output", "alonge_sra_ids.txt"),
+#   quote = FALSE,
+#   row.names = FALSE,
+#   col.names = FALSE
+# )
 
+
+# Linking SRA ids to CW identifiers ---------------------------------------
+# Right now the SRA ids are just linked to whatever accession identifier the 
+# authors used, which are inconsistent and confusing. I'll try to link them 
+# to CW ids here.
+
+# Loading the metadata sheet
+metadata <- read_sheet("1V2kH8G4tfYsYqnYb6bVHpGjwwkjd9le0arGBJ2o4r8s") %>%
+  select(wave:name_original_razifard)
+
+sra_info_varitome <- varitome_metadata %>%
+  select(Run, Sample.Name)
+
+sra_info_alonge <- alonge_metadata_output %>%
+  select(Run, Sample.Name)
+
+# Joining in the CW identifiers
+sra_info_varitome <- sra_info_varitome %>%
+  left_join(metadata, join_by(Sample.Name == name_original_razifard))
